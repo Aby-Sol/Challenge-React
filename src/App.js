60a4024a816d08baa290392ff4efe6aed56b2368
Aby-Sol <Aby.soltani@gmail.Com>
2024-01-11 17:37:03 +0100

    adding files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar'
+import Header from './Header'
+import Menu from './Menu'
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Header/>
+     <Menu/>
+    </div>
+  );
+}
+
+export default App;
